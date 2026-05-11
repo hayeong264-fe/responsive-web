d8ebe0fb6976159aaa31f8dc64c34672d052cbc0
hayeong <hayeong264@gmail.com>
2026-05-11 14:57:53 +0900

    주제별 스크립트 완료

---
+++ scripts/subject.js
@@ -0,0 +1,19 @@
+console.log("연결됨");
+const tabBtn = document.querySelectorAll(".icon-btn li");
+const content = document.querySelectorAll(".kdc-box .ch");
+
+tabBtn.forEach(function(btn, index){
+
+    btn.addEventListener("click", function(){
+
+        content.forEach(function(item){
+            item.classList.remove("show");
+        });
+
+        if(content[index]){
+            content[index].classList.add("show");
+        }
+
+    });
+
+});
